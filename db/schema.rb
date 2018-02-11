2dc10270ee5f0707e945cdc26087a29f9be00f04
Artem Rakov <scoff1k@gmail.com>
2018-02-11 14:30:10 +0000

    generated model and controller for fooditems

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180120151524) do
+ActiveRecord::Schema.define(version: 20180211142732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "food_items", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
