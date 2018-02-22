43f5b4e23ca9103b8b97dd2ecea211ca80171d47
Artem Rakov <scoff1k@gmail.com>
2018-02-22 15:13:08 +0000

    added navbar for the menu and styled it

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,10 +1,44 @@
 <template>
-    <div>
-        <ul>
-            <li v-for="item in items" :key="item.id"> 
-                {{ item.name }}
-            </li>
-        </ul>
+    <div class="menu">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="menu__navbar">
+                        <ul class="menu__list">
+                            <li class="menu__navbar_item">
+                               <a href="#"> Сандвичи </a> 
+                            </li>
+                            <li class="menu__navbar_item">
+                               <a href="#"> Роллы  </a> 
+                            </li>
+                            <li class="menu__navbar_item">
+                               <a href="#"> Картофель  </a> 
+                            </li>
+                            <li class="menu__navbar_item">
+                               <a href="#"> Стартеры  </a> 
+                            </li>
+                            <li class="menu__navbar_item">
+                               <a href="#"> Соусы  </a> 
+                            </li>
+                            <li class="menu__navbar_item">
+                               <a href="#">  Напитки </a> 
+                            </li>
+                            <li class="menu__navbar_item">
+                               <a href="#">  Десерты </a> 
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-5">
+                    <div class="basket__container">
+                        <div class="basket">
+                            <a href="#" class="basket__btn"> Сделать заказ </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -17,3 +51,10 @@ export default {
 <style>
 
 </style>
+
+
+// <ul>
+//             <li v-for="item in items" :key="item.id"> 
+//                 {{ item.name }}
+//             </li>
+//         </ul>
