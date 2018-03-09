0898d6a0204abf4a229a77b47bf013c658779216
Artem Rakov <scoff1k@gmail.com>
2018-03-09 16:32:42 +0000

    created basket

---
+++ app/models/basket.rb
@@ -0,0 +1,4 @@
+class Basket < ApplicationRecord
+  belongs_to :order
+  belongs_to :food_item
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309114020) do
+ActiveRecord::Schema.define(version: 20180309163151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "baskets", force: :cascade do |t|
+    t.bigint "order_id"
+    t.bigint "food_item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_item_id"], name: "index_baskets_on_food_item_id"
+    t.index ["order_id"], name: "index_baskets_on_order_id"
+  end
 
   create_table "delivery_guys", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -58,4 +67,6 @@ ActiveRecord::Schema.define(version: 20180309114020) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "baskets", "food_items"
+  add_foreign_key "baskets", "orders"
 end
