57ef9740393a62e004a65b795d393667a1ce8cc3
Artem Rakov <scoff1k@gmail.com>
2018-03-09 15:47:26 +0000

    added optional true for order and added enum

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
-  belongs_to :user, class_name: "User", foreign_key: "user_id"
+  belongs_to :user, class_name: "User", foreign_key: "user_id", optional: true
   belongs_to :delivery_guy, class_name: "User", foreign_key: "delivery_guy_id"
+
+  enum status: { submitted: 0, approved: 1, delivered: 2, rejected: 3 }
+
 end
